488a2ffbf396397e96fa76ec5c952f06ddeb386c
Soumyadeep <mannasoumyadeep@gmail.com>
2025-02-14 16:09:58 +0530

    Fix merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './globals.css';
-<<<<<<< HEAD
-=======
 import './styles.css';
->>>>>>> 126311a226d8285876475ba0d14da72eb156fb72
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
->>>>>>> 126311a226d8285876475ba0d14da72eb156fb72
